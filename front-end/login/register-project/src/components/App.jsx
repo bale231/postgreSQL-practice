b9be6bb32c3e8ddc12b745a009fcc3387459682e
Luigi Balestrucci <card3001@outlook.it>
2024-06-13 21:26:22 +0200

    Add Routing in front-end.

---
+++ front-end/login/register-project/src/components/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./Navbar";
+import React from "react";
+import Footer from "./Footer";
+import Homepage from "./pages/Homepage";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Homepage />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
