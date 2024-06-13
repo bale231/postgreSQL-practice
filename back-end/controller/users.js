fb036af4e2d5b12659561fbf81367c2d30158cef
Luigi Balestrucci <card3001@outlook.it>
2024-06-13 18:18:14 +0200

    Add controllers to clean code

---
+++ back-end/controller/users.js
@@ -0,0 +1,64 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+import { db } from '../database.js'
+
+const getUsers = async (req, res) => {
+    try {
+        const psqlQuery = "SELECT * FROM users";
+        const users = await db.many(psqlQuery);
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ msg: "Error retrieving users", error: error.message });
+    }
+}
+
+const register = async (req, res) => {
+    const { username, email, password, birth_date } = req.body;
+    const role_id = 2;
+    const hashedPassword = await bcrypt.hash(password, 10); // Hash la password
+
+    try {
+        await db.none(
+            `INSERT INTO users (username, email, password, birth_date, role_id) 
+            VALUES($1, $2, $3, $4, $5)`,
+            [username, email, hashedPassword, birth_date, role_id]
+        );
+        res.status(201).json({ msg: "User registered successfully!!", user: { username, email, birth_date, role_id } });
+    } catch (error) {
+        res.status(500).json({ msg: "Error registering user", error: error.message });
+    }
+}
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await db.oneOrNone(`SELECT * FROM users WHERE email = $1`, [email]);
+
+        if (!user) {
+            return res.status(404).json({ msg: "User not found!" });
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (passwordMatch) {
+            const payload = {
+                id: user.id,
+                email,
+            };
+            const { SECRET_KEY } = process.env;
+            const token = jwt.sign(payload, SECRET_KEY);
+
+            res.status(200).json({ msg: "User logged in successfully!", id: user.id, email, token });
+        } else {
+            return res.status(401).json({ msg: "Password is incorrect!" });
+        }
+    } catch (error) {
+        console.error('Login error:', error); // Log l'errore per debug
+        res.status(500).json({ msg: "Error during login", error: error.message });
+    }
+}
+
+export { getUsers, register, login }
